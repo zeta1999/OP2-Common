fe09a1d2ea6c65321c0391ce02e8999e9f8fe80f
gihanmudalige <gihan.mudalige@oerc.ox.ac.uk>
2012-11-27 16:10:20 +0000

    Adding initial files to creare static fortran code generator

---
+++ translator/fortran/python/op2_fortran.py
@@ -0,0 +1,381 @@
+#!/usr/bin/python
+#
+# OP2 source code transformation tool
+#
+# This tool parses the user's original source code to produce
+# target-specific code to execute the user's kernel functions.
+#
+# This prototype is written in Python and is directly based on the
+# parsing and code generation of the matlab source code transformation code
+#
+# usage: op2('file1','file2',...)
+#
+# This code generator is for parsing applications written using the OP2 FORTRAN API
+#
+# This takes as input
+#
+# file1.F90, file2.F90, ...
+#
+# and produces as output modified versions .....
+#
+# file1_op.F90, file2_op.F90, ...
+#
+# then calls a number of target-specific code generators
+# to produce individual kernel files of the form
+#
+# xxx_kernel.??  -- for OpenMP x86 execution
+# xxx_kernel.??   -- for CUDA execution
+#
+# plus a master kernel file of the form
+#
+# file1_kernels.??  -- for OpenMP x86 execution
+# file1_kernels.??   -- for CUDA execution
+
+import sys
+import re
+import datetime
+
+
+#
+# declare constants
+#
+
+ninit = 0; nexit = 0; npart = 0; nhdf5 = 0; nconsts  = 0; nkernels = 0;
+consts = []
+kernels = []
+
+OP_ID   = 1;  OP_GBL   = 2;  OP_MAP = 3;
+
+OP_READ = 1;  OP_WRITE = 2;  OP_RW  = 3;
+OP_INC  = 4;  OP_MAX   = 5;  OP_MIN = 6;
+
+OP_accs_labels = ['OP_READ','OP_WRITE','OP_RW','OP_INC','OP_MAX','OP_MIN' ]
+
+
+##########################################################################
+# Remove comments from text
+##########################################################################
+
+def comment_remover(text):
+		def replacer(match):
+				s = match.group(0)
+				if s.startswith('/'):
+						return ""
+				else:
+						return s
+		pattern = re.compile(
+				r'//.*?$|/\*.*?\*/|\'(?:\\.|[^\\\'])*\'|"(?:\\.|[^\\"])*"',
+				re.DOTALL | re.MULTILINE
+		)
+		return re.sub(pattern, replacer, text)
+#end of comment_remover(text)
+
+##########################################################################
+# parsing for op_init/op_exit/op_partition/op_hdf5 calls
+##########################################################################
+
+def op_parse_calls(text):
+
+		#remove comments just for this call
+		text = comment_remover(text)
+
+		inits = 0
+		search = "op_init"
+		found = text.find(search)
+		while found > -1:
+			found=text.find(search, found+1)
+			inits = inits + 1
+
+		exits = 0
+		search = "op_exit"
+		found = text.find(search)
+		while found > -1:
+			found=text.find(search, found+1)
+			exits = exits + 1
+
+		parts = 0
+		search = "op_partition"
+		found = text.find(search)
+		while found > -1:
+			found=text.find(search, found+1)
+			parts = parts + 1
+
+		hdf5s = 0
+		search = "hdf5"
+		found = text.find(search)
+		while found > -1:
+			found=text.find(search, found+1)
+			hdf5 = hdf5s + 1
+
+		return (inits, exits, parts, hdf5s)
+#end of op_parse_calls(text)
+
+
+##########################################################################
+# parsing for op_decl_const calls
+##########################################################################
+
+def op_decl_const_parse(text):
+		consts = []
+		num_const = 0
+		search = "op_decl_const"
+		i = text.find(search)
+		while i > -1:
+			const_string = text[text.find('(',i)+1: text.find(')',i+13)]
+			#print 'Args found at index i : '+const_string
+
+			#remove comments
+			const_string = comment_remover(const_string)
+
+			#check for syntax errors
+			if len(const_string.split(',')) <> 3:
+				print 'Error in op_decl_const : must have three arguments'
+				return
+
+			temp = {'loc': i,
+				'dim':const_string.split(',')[0],
+				'type': const_string.split(',')[1],
+				'name':const_string.split(',')[2],
+				'name2':const_string.split(',')[2]}
+
+			consts.append(temp)
+
+			i=text.find(search, i+13)
+			num_const = num_const + 1
+
+		return (consts)
+#end of op_decl_const_parse
+
+##########################################################################
+# parsing for arguments in op_par_loop to find the correct closing brace
+##########################################################################
+def arg_parse(text,j):
+
+		depth = 0
+		loc2 = j;
+		while 1:
+			if text[loc2] == '(':
+				depth = depth + 1
+
+			elif text[loc2] == ')':
+				depth = depth - 1
+				if depth == 0:
+					return loc2
+			loc2 = loc2 + 1
+#end of arg_parse
+
+def find_all(string, occurrence):
+		found = 0
+		while True:
+			found = string.find(occurrence, found)
+			if found != -1:
+				yield found
+			else:
+				break
+			found += 1
+#end of find_all
+
+
+def get_arg_dat(arg_string, j):
+	loc = arg_parse(arg_string,j+1)
+	dat_args_string = arg_string[arg_string.find('(',j)+1:loc]
+	#print dat_args_string
+
+	#remove comments
+	dat_args_string = comment_remover(dat_args_string)
+
+	#check for syntax errors
+	if len(dat_args_string.split(',')) <> 6:
+		print 'Error in parsing op_arg_dat('+ dat_args_string +'): must have six arguments'
+		return
+
+	#split the dat_args_string into  6 and create a struct with the elements and type as op_arg_dat
+	temp_dat = {'type':'op_arg_dat',
+	'dat':dat_args_string.split(',')[0].strip(),
+	'idx':dat_args_string.split(',')[1].strip(),
+	'map':dat_args_string.split(',')[2].strip(),
+	'dim':dat_args_string.split(',')[3].strip(),
+	'typ':dat_args_string.split(',')[4].strip(),
+	'acc':dat_args_string.split(',')[5].strip()}
+
+	return temp_dat
+#end of get_arg_dat
+
+
+def get_arg_gbl(arg_string, k):
+	loc = arg_parse(arg_string,k+1)
+	gbl_args_string = arg_string[arg_string.find('(',k)+1:loc]
+	#print gbl_args_string
+
+	#remove comments
+	gbl_args_string = comment_remover(gbl_args_string)
+
+	#check for syntax errors
+	if len(gbl_args_string.split(',')) <> 4:
+		print 'Error in parsing op_arg_gbl('+ gbl_args_string +'): must have four arguments'
+		return
+
+	#split the gbl_args_string into  4 and create a struct with the elements and type as op_arg_gbl
+	temp_gbl = {'type':'op_arg_gbl',
+	'data':gbl_args_string.split(',')[0].strip(),
+	'dim':gbl_args_string.split(',')[1].strip(),
+	'typ':gbl_args_string.split(',')[2].strip(),
+	'acc':gbl_args_string.split(',')[3].strip()}
+
+	return temp_gbl
+#end of get_arg_gbl
+
+##########################################################################
+# parsing for op_par_loop calls
+##########################################################################
+
+def op_par_loop_parse(text):
+		loop_args = []
+
+		search = "op_par_loop"
+		i = text.find(search)
+		while i > -1:
+			arg_string = text[text.find('(',i)+1:text.find(';',i+11)]
+			print arg_string
+
+			#parse arguments in par loop
+			temp_args = []
+			num_args = 0
+
+			#try:
+			#parse each op_arg_dat
+			search2 = "op_arg_dat"
+			search3 = "op_arg_gbl"
+			j = arg_string.find(search2)
+			k = arg_string.find(search3)
+
+			while j > -1 or k > -1:
+				if  k <= -1 :
+					temp_dat = get_arg_dat(arg_string,j)
+					#append this struct to a temporary list/array
+					temp_args.append(temp_dat)
+					num_args = num_args + 1
+					j= arg_string.find(search2, j+11)
+
+				elif  j <= -1 :
+					temp_gbl = get_arg_gbl(arg_string,k)
+					#append this struct to a temporary list/array
+					temp_args.append(temp_gbl)
+					num_args = num_args + 1
+					k= arg_string.find(search3, k+11)
+
+				elif j < k:
+					temp_dat = get_arg_dat(arg_string,j)
+					#append this struct to a temporary list/array
+					temp_args.append(temp_dat)
+					num_args = num_args + 1
+					j= arg_string.find(search2, j+11)
+
+				else:
+					temp_gbl = get_arg_gbl(arg_string,k)
+					#append this struct to a temporary list/array
+					temp_args.append(temp_gbl)
+					num_args = num_args + 1
+					k= arg_string.find(search3, k+11)
+
+			temp = {'loc':i,
+			'name1':arg_string.split(',')[0].strip(),
+			'name2':arg_string.split(',')[1].strip(),
+			'set':arg_string.split(',')[2].strip(),
+			'args':temp_args,
+			'nargs':num_args}
+
+			loop_args.append(temp)
+			i=text.find(search, i+10)
+		#print loop_args
+		print '\n\n'
+		return (loop_args)
+
+###################END OF FUNCTIONS DECLARATIONS #########################
+
+
+
+##########################################################################
+#                      ** BEGIN MAIN APPLICATION **
+##########################################################################
+
+#####################loop over all input source files#####################
+for a in range(1,len(sys.argv)):
+	print 'processing file '+ str(a) + ' of ' + str(len(sys.argv)-1) + ' '+ \
+	str(sys.argv[a])
+
+	src_file = str(sys.argv[a])
+	f = open(src_file,'r')
+	text = f.read()
+
+############ check for op_init/op_exit/op_partition/op_hdf5 calls ########
+
+	inits, exits, parts, hdf5s = op_parse_calls(text)
+
+	if inits+exits+parts+hdf5s > 0:
+		print ' '
+	if inits > 0:
+		print'contains op_init call'
+	if exits > 0:
+		print'contains op_exit call'
+	if parts > 0:
+		print'contains op_partition call'
+	if hdf5s > 0:
+		print'contains op_hdf5 calls'
+
+	ninit = ninit + inits;
+	nexit = nexit + exits;
+	npart = npart + parts;
+	nhdf5 = nhdf5 + hdf5s;
+
+########################## parse and process constants ###################
+
+	const_args = op_decl_const_parse(text)
+
+	#cleanup '&' symbols from name and convert dim to integer
+	for i  in range(0,len(const_args)):
+		if const_args[i]['name'][0] == '&':
+			const_args[i]['name'] = const_args[i]['name'][1:]
+			const_args[i]['dim'] = int(const_args[i]['dim'])
+
+	#check for repeats
+	nconsts = 0
+	for i  in range(0,len(const_args)):
+		repeat = 0
+		name = const_args[i]['name']
+		for c in range(0,nconsts):
+			if  const_args[i]['name'] == consts[c]['name']:
+				repeat = 1
+				if const_args[i]['type'] <> consts[c]['type']:
+					print 'type mismatch in repeated op_decl_const'
+				if const_args[i]['dim'] <> consts[c]['dim']:
+					print 'size mismatch in repeated op_decl_const'
+
+		if repeat > 0:
+			print 'repeated global constant ' + const_args[i]['name']
+		else:
+			print '\nglobal constant ('+ const_args[i]['name'].strip() + ') of size ' \
+			+ str(const_args[i]['dim'])
+
+	#store away in master list
+		if repeat == 0:
+			nconsts = nconsts + 1
+			temp = {'dim': const_args[i]['dim'],
+			'type': const_args[i]['type'].strip(),
+			'name': const_args[i]['name'].strip()}
+			consts.append(temp)
+
+
+#################### parse and process op_par_loop calls #################
+
+	loop_args = op_par_loop_parse(text)
+
+	for i in range (0, len(loop_args)):
+		name = loop_args[i]['name1']
+		nargs = loop_args[i]['nargs']
+		print '\nprocessing kernel '+name+' with '+str(nargs)+' arguments',
+
+
+##########################################################################
+#                      ** END MAIN APPLICATION **
+##########################################################################
