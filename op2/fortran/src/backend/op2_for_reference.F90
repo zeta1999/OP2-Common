bde3e3641efcbc03014cedb83eb30a234504675f
Carlo Bertolli <c.bertolli@imperial.ac.uk>
2012-12-12 18:43:07 +0000

    Fix bug in reference serial implementation (core size 0, exec halo size non-zero)

--- op2/fortran/src/backend/op2_for_reference.F90
+++ op2/fortran/src/backend/op2_for_reference.F90
@@ -35,6 +35,26 @@ OP_LOOP(21) OP_LOOP(22) OP_LOOP(23) OP_LOOP(24) OP_LOOP(25) OP_LOOP(26) OP_LOOP(
 OP_LOOP(31) OP_LOOP(32) OP_LOOP(33) OP_LOOP(34) OP_LOOP(35) OP_LOOP(36) OP_LOOP(37) OP_LOOP(38) OP_LOOP(39) OP_LOOP(40)
 OP_LOOP(41) OP_LOOP(42)
 
+
+  subroutine op_par_loop_accu_f(kernel, set, &
+  arg1 &
+ , arg2 &
+ , arg3 &
+  ) BIND(C,name="op_par_loop_accu")
+  use, intrinsic :: ISO_C_BINDING
+ import :: op_set_core, op_arg
+interface
+ subroutine kernel () BIND(C)
+ end subroutine kernel
+end interface
+ type(op_set_core) :: set
+ type(op_arg) :: arg1
+ type(op_arg) :: arg2
+ type(op_arg) :: arg3
+
+end subroutine op_par_loop_accu_f
+
+
 end interface
 
   contains
@@ -60,5 +80,23 @@ OP_LOOP2(21) OP_LOOP2(22) OP_LOOP2(23) OP_LOOP2(24) OP_LOOP2(25) OP_LOOP2(26) OP
 OP_LOOP2(31) OP_LOOP2(32) OP_LOOP2(33) OP_LOOP2(34) OP_LOOP2(35) OP_LOOP2(36) OP_LOOP2(37) OP_LOOP2(38) OP_LOOP2(39) OP_LOOP2(40)
 OP_LOOP2(41) OP_LOOP2(42)
 
-end module OP2_Fortran_Reference
+ subroutine op_par_loop_accu(kernel, set, &
+ arg1 &
+, arg2 &
+, arg3 &
+ )
+ external kernel
+ type(op_set) :: set
+ type(op_arg) :: arg1
+ type(op_arg) :: arg2
+ type(op_arg) :: arg3
 
+ call op_par_loop_accu_f(kernel, set%setPtr, &
+ arg1 &
+, arg2 &
+, arg3 &
+ )
+end subroutine op_par_loop_accu
+
+
+end module OP2_Fortran_Reference
