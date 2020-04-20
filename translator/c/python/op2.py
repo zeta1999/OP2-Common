ad039c2e0747cc969df69afd057fc924d54157de
sikba <siklosi.balint@hallgato.ppke.hu>
2020-04-20 20:43:41 +0200

    halfway working

--- translator/c/python/op2.py
+++ translator/c/python/op2.py
@@ -812,8 +812,8 @@ def main(srcFilesAndDirs=sys.argv[1:]):
     fid.close()
   # end of loop over input source files
 
-  ## Loop over kernels, looking for a header file named after each 
-  ## kernel in either working directory or one of the input-supplied 
+  ## Loop over kernels, looking for a header file named after each
+  ## kernel in either working directory or one of the input-supplied
   ## directories:
   for nk in xrange(0, len(kernels)):
     k_data = kernels[nk]
@@ -836,9 +836,9 @@ def main(srcFilesAndDirs=sys.argv[1:]):
             k_data["decl_filepath"] = filepath
             break
 
-  ## Any kernel declarations still not found must exist in files 
-  ## not named after the kernel. Search through content of all 
-  ## input-supplied files, and through all files of input-supplied 
+  ## Any kernel declarations still not found must exist in files
+  ## not named after the kernel. Search through content of all
+  ## input-supplied files, and through all files of input-supplied
   ## directories:
   for nk in xrange(0, len(kernels)):
     if not "decl_filepath" in kernels[nk].keys():
@@ -904,18 +904,19 @@ def main(srcFilesAndDirs=sys.argv[1:]):
 
   #code generators for OpenMP parallelisation with MPI
   #op2_gen_openmp(masterFile, date, consts, kernels) # Initial OpenMP code generator
-  op2_gen_openmp_simple(masterFile, date, consts, kernels) # Simplified and Optimized OpenMP code generator
-  op2_gen_openacc(masterFile, date, consts, kernels) # Simplified and Optimized OpenMP code generator
+#  op2_gen_openmp_simple(masterFile, date, consts, kernels) # Simplified and Optimized OpenMP code generator
+#  op2_gen_openacc(masterFile, date, consts, kernels) # Simplified and Optimized OpenMP code generator
 
   #code generators for NVIDIA GPUs with CUDA
   #op2_gen_cuda(masterFile, date, consts, kernels,sets) # Optimized for Fermi GPUs
-  op2_gen_cuda_simple(masterFile, date, consts, kernels, sets, macro_defs) # Optimized for Kepler GPUs
+#  op2_gen_cuda_simple(masterFile, date, consts, kernels, sets, macro_defs) # Optimized for Kepler GPUs
 
   # generates openmp code as well as cuda code into the same file
-  op2_gen_cuda_simple_hyb(masterFile, date, consts, kernels, sets) # CPU and GPU will then do comutations as a hybrid application
+#  op2_gen_cuda_simple_hyb(masterFile, date, consts, kernels, sets) # CPU and GPU will then do comutations as a hybrid application
 
   #code generator for GPUs with OpenMP4.5
-  op2_gen_openmp4(masterFile, date, consts, kernels)
+#  op2_gen_openmp4(masterFile, date, consts, kernels)
+
 
   # import subprocess
   # retcode = subprocess.call("which clang-format > /dev/null", shell=True)
